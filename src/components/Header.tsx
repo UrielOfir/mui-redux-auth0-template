bdb2da379464dd49b40c3e43663d0fc2ddb2bc8c
giladfuchs <igilfu@gmail.com>
2024-02-26 14:21:35 +0200

    add sign in & logout to avatar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import {AppBar, Grid,  Toolbar, Typography} from '@mui/material';
 import ProfileAvatar from "@/app/ProfileAvatar";
-import {useSession} from "next-auth/react";
-const image_url = "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+
 function Header() {
-    const { data: session }: {data: any} = useSession()
 
   return (
     <AppBar position="sticky">
@@ -17,7 +15,7 @@ function Header() {
               </Grid>
               <Grid item xs />
               <Grid item>
-                  <ProfileAvatar imageUrl={(session && session.user.image) || image_url}/>
+                  <ProfileAvatar />
               </Grid>
           </Grid>
       </Toolbar>
